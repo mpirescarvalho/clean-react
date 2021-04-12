ad1ff4d7829bfe546975ba5abdac99dcb38734a6
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-04-12 19:31:18 -0300

    chore: add react and sass environment

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: { '.*\\.ts$': 'ts-jest' },
+  testEnvironment: 'jsdom',
+  transform: { '.*\\.(ts|tsx)$': 'ts-jest' },
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
