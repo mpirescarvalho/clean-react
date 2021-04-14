80ecef238d58e2c9b491313aa86513ee1bf8bd41
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-04-14 00:59:51 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styles from './styles.scss';
+
+type Props = React.HTMLAttributes<HTMLElement>
+
+const Spinner: React.FC<Props> = (props) => {
+  return (
+    <div {...props} className={[Styles.spinner, props.className].join(' ')}>
+      <div /> <div /> <div /> <div />
+    </div>
+  )
+}
+
+export default Spinner;
--- webpack.config.js
+++ webpack.config.js
@@ -10,29 +10,39 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.scss'],
+    extensions: ['.ts', '.tsx', '.js', '.scss', '.svg'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
   },
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader',
-        options: {
-          modules: true
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        }, {
+          loader: 'sass-loader'
+        }]
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {}
         }
-      }, {
-        loader: 'sass-loader'
-      }]
-    }]
+      }
+    ]
   },
   devServer: {
     contentBase: './public',
