5d693b4e78173dc968da8031557357c139466ab8
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-04-27 21:02:14 -0300

    refactor: clean login tests

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -26,11 +26,11 @@ const Login: React.FC<LoginProps> = ({ validation }) => {
 	});
 
 	useEffect(() => {
-		validation.validate({ email: state.email });
+		validation.validate("email", state.email);
 	}, [state.email]);
 
 	useEffect(() => {
-		validation.validate({ password: state.password });
+		validation.validate("password", state.password);
 	}, [state.password]);
 
 	return (
--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,27 +1,18 @@
+import Login from "./index";
+import { ValidationSpy } from "@/presentation/test";
 import React from "react";
-import { Validation } from "@/presentation/protocols/validation";
 import {
 	render,
 	RenderResult,
 	fireEvent,
 	cleanup,
 } from "@testing-library/react";
-import Login from "./index";
+import faker from "faker";
 
 type SutTypes = {
 	sut: RenderResult;
 	validationSpy: ValidationSpy;
 };
-
-class ValidationSpy implements Validation {
-	errorMessage: string = "";
-	input: object = {};
-
-	validate(input: object): string {
-		this.input = input;
-		return this.errorMessage;
-	}
-}
 
 const makeSut = (): SutTypes => {
 	const validationSpy = new ValidationSpy();
@@ -52,18 +43,18 @@ describe("Login Component", () => {
 	test("Should call Validation with correct email", () => {
 		const { sut, validationSpy } = makeSut();
 		const emailInput = sut.getByTestId("email");
-		fireEvent.input(emailInput, { target: { value: "any_email" } });
-		expect(validationSpy.input).toEqual({
-			email: "any_email",
-		});
+		const email = faker.internet.email();
+		fireEvent.input(emailInput, { target: { value: email } });
+		expect(validationSpy.fieldName).toBe("email");
+		expect(validationSpy.fieldValue).toBe(email);
 	});
 
 	test("Should call Validation with correct password", () => {
 		const { sut, validationSpy } = makeSut();
 		const passwordInput = sut.getByTestId("password");
-		fireEvent.input(passwordInput, { target: { value: "any_password" } });
-		expect(validationSpy.input).toEqual({
-			password: "any_password",
-		});
+		const password = faker.internet.password();
+		fireEvent.input(passwordInput, { target: { value: password } });
+		expect(validationSpy.fieldName).toBe("password");
+		expect(validationSpy.fieldValue).toBe(password);
 	});
 });
